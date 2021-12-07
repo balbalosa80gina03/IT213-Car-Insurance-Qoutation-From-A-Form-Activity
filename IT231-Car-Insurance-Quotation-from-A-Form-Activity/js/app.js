c4e3fe2eac8d1294ed3a7c561fe8ab3cce52e2a8
balbalosa80gina03 <balbalosa80gina03@gmail.com>
2021-12-07 20:51:07 +0800

    added a code creating different years in app.js
    Signed-off-by: balbalosa80gina03 <balbalosa80gina03@gmail.com>

--- IT231-Car-Insurance-Quotation-from-A-Form-Activity/js/app.js
+++ IT231-Car-Insurance-Quotation-from-A-Form-Activity/js/app.js
@@ -1,1 +1,37 @@
 
+
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const html = new HTMLUListElement();
+    html.displayYears();
+
+});
+
+
+
+
+
+function HTMLUI() {}
+
+
+HTMLUI.prototype.displayYears = function() {
+
+    const max = new Date().getFullYears(),
+          min = max - 20;
+
+    const selectYears = document.getElementById('year');
+
+
+
+    for (let i = max; i > min; i--) {
+        const option = document.createElement('option');
+        option.value = i;
+        option.textContent =i;
+        selectYears.appendChild(option);
+    }
+}
